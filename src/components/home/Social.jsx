3e8bb39678f23e5131c24b54f2d4c4ce84dbc807
unknown <arnavawasthy60@gmail.com>
2024-02-26 19:17:09 +0530

    created css vars and home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a 
+        href="" 
+        className="home__social-icon" 
+        target='_blank'>
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        <a 
+        href="https://www.linkedin.com/in/arnav-awasthi-a39148159/" 
+        className="home__social-icon" 
+        target='_blank'>
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a 
+        href="https://github.com/Arnav60" 
+        className="home__social-icon" 
+        target='_blank'>
+        <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
